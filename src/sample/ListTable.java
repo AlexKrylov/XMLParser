abc73350dd777df6480f4d2dfe7e0927275e3b81
AlexKrylov <slate_86@mail.ru>
2018-08-27 15:49:55 +0300

    Closed BufferedReaders

--- src/sample/ListTable.java
+++ src/sample/ListTable.java
@@ -19,6 +19,7 @@ public class ListTable {
                 }
             }
         }
+        reader.close();
         return list;
     }
 }
--- src/sample/Reader.java
+++ src/sample/Reader.java
@@ -24,6 +24,7 @@ public class Reader {
                 list.add(str);
             }
         }
+        reader.close();
         return list;
     }
 
